5b67db5f26cf78f3a9d7a69de076dfc73a28475d
Maturi Praveen kumar <praveenmaturi03@gmail.com>
2026-05-27 17:50:32 +0530

    Add files via upload

---
+++ Day-2/variables.py
@@ -0,0 +1,46 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/prave/OneDrive/Desktop/Python-course-work/Day-2/keyword1.py
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+a=5
+>>> b=6
+>>> print(a+b)
+11
+>>> myvar1=10
+>>> my_var=10
+>>> my var=10
+SyntaxError: invalid syntax
+>>> myvar1
+10
+>>> my_a=15
+>>> _b=67
+>>> my_a*_b
+1005
+>>> a=98
+>>> b=15
+>>> a%b
+8
+>>> 1myvar=10
+SyntaxError: invalid decimal literal
+>>> var=10
+>>> var=20
+>>> var
+20
+>>> a=b=c=10
+>>> a
+10
+>>> b
+10
+>>> c
+10
+>>> a,b,c,d=12,13,14,15
+>>> a
+12
+>>> c
+14
+>>> b
+13
+>>> d
+15
