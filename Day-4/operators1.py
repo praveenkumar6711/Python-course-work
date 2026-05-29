184ef398e98f92df22ca6c304fd6ef1ed9892e77
Maturi Praveen kumar <praveenmaturi03@gmail.com>
2026-05-29 18:02:22 +0530

    Add files via upload

---
+++ Day-4/operators1.py
@@ -0,0 +1,49 @@
+arthematic
+comaprision
+assesment
+logical
+Membership Operators
+Identity Operators
+bitwise
+
+day-4
+
+operators in python
+
+
+Arthematic ->+,-./,%,*,**
+
+comaprision: < > <=,>=,==,!=
+
+assessment:  =	x = 5
++=	x += 2
+-=	x -= 2
+*=	x *= 2
+/=	x /= 2
+%=	x %= 2
+**=	x **= 2
+//=     x //= 2
+logical:
+    
+Operator	Meaning
+and	True if both conditions are true
+or	True if at least one condition is true
+not	Reverses the result
+
+Membership Operators:Operator	Meaning
+in	Present
+not in	Not Present
+
+Identity Operators:
+Operator	Meaning
+is	Same object
+is not	Different object
+
+bitwise:
+Operator	Meaning
+&	AND
+`	`
+^	XOR
+~	NOT
+<<	Left Shift
+>>	Right Shift
