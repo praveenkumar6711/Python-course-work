3609af4911fd680c2b40eabe4b471a18c1d362b2
Maturi Praveen kumar <praveenmaturi03@gmail.com>
2026-06-13 19:56:01 +0530

    Add files via upload

---
+++ Day-14/A-Z.py
@@ -0,0 +1,169 @@
+
+'''
+
+n = int(input("Enter the size: "))
+m=n//2
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==m or j==0 or j==n-1:
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
+Enter the size: 11
+* * * * * * * * * * * 
+*                   * 
+*                   * 
+*                   * 
+*                   * 
+* * * * * * * * * * * 
+*                   * 
+*                   * 
+*                   * 
+*                   * 
+*                   *
+
+
+
+
+n = int(input("Enter the size: "))
+m=n//2
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==m or i==n-1 or j==0 or j==n-1:
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
+
+
+Enter the size: 11
+* * * * * * * * * * * 
+*                   * 
+*                   * 
+*                   * 
+*                   * 
+* * * * * * * * * * * 
+*                   * 
+*                   * 
+*                   * 
+*                   * 
+* * * * * * * * * * *
+
+
+
+n = int(input("Enter the size: "))
+m=n//2
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0:
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
+
+Enter the size: 11
+* * * * * * * * * * * 
+*                     
+*                     
+*                     
+*                     
+*                     
+*                     
+*                     
+*                     
+*                     
+* * * * * * * * * * *
+
+
+
+n = int(input("Enter the size: "))
+m=n//2
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j==n-1:
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
+Enter the size: 11
+* * * * * * * * * * * 
+*                   * 
+*                   * 
+*                   * 
+*                   * 
+*                   * 
+*                   * 
+*                   * 
+*                   * 
+*                   * 
+* * * * * * * * * * *
+
+
+
+n = int(input("Enter the size: "))
+m=n//2
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or i==m or j==0 :
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
+
+Enter the size: 11
+* * * * * * * * * * * 
+*                     
+*                     
+*                     
+*                     
+* * * * * * * * * * * 
+*                     
+*                     
+*                     
+*                     
+* * * * * * * * * * * 
+
+
+
+n = int(input("Enter the size: "))
+m=n//2
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==m or j==0 :
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
+
+Enter the size: 11
+* * * * * * * * * * * 
+*                     
+*                     
+*                     
+*                     
+* * * * * * * * * * * 
+*                     
+*                     
+*                     
+*                     
+* 
+
+'''
+n = int(input("Enter the size: "))
+m=n//2
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or  (i==m and 
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
+
