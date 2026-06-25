94962ea94a737f2e7e4951f8507f69b83cf372e3
Maturi Praveen kumar <praveenmaturi03@gmail.com>
2026-06-25 12:52:33 +0530

    Add files via upload

---
+++ Day-24/encapsulaton.py
@@ -0,0 +1,63 @@
+
+
+
+'''
+class Instagram:
+    def __init__(self, username, password):
+        self.username = username
+        self.__password = password
+        self.followers = []
+
+    def getpassword(self):
+        return self.__password
+
+    def setpassword(self, newpassword):
+        self.__password = newpassword
+
+
+praveen = Instagram("praveen", "Praveen@123")
+
+# Username
+print("Before username:", praveen.username)
+praveen.username = "praveenkumar"
+print("After username:", praveen.username)
+
+# Password
+print("Before password:", praveen.getpassword())
+praveen.setpassword("praveenkumar@123")
+print("After password:", praveen.getpassword())
+
+
+Before username: praveen
+After username: praveenkumar
+Before password: Praveen@123
+After password: praveenkumar@123
+'''
+'''
+class Instagram:
+    def __init__(self):
+        self._post = []
+
+    @property
+    def accesspost(self):
+        return self._post
+
+    @accesspost.setter
+    def accesspost(self,newpost):
+        self._post.append(newpost)
+
+praveen=Instagram()
+print(praveen.accesspost)
+praveen.accesspost = 'class and object'
+print(praveen.accesspost)
+
+
+[]
+['class and object']
+
+
+'''
+
+
+
+
